c751dc505cccbe2c354638d3e55d961e1c35cf75
ogunmolu oluwaseun <58033364+lagrandecode@users.noreply.github.com>
2022-03-05 13:24:23 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,29 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.yellow,
       body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 10, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:  [
+              SizedBox(height: 50),
+              Text("score: 0", style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),),
+              SizedBox(height: 10,),
+              Text("00:00", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),),
+              SizedBox(height: 30,),
+              Container(
+                height: 100,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.zero,bottomRight: Radius.circular(10))
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
 
       )
     );
