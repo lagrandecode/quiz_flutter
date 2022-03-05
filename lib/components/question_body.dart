fc1f34f39bdd7dd350a4ef6607801db4d5026664
ogunmolu oluwaseun <58033364+lagrandecode@users.noreply.github.com>
2022-03-05 14:09:17 +0100

    commit

--- lib/components/question_body.dart
+++ lib/components/question_body.dart
@@ -11,14 +11,18 @@ class QuestionBody extends StatefulWidget {
 class _QuestionBodyState extends State<QuestionBody> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Colors.grey,
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.zero,bottomRight: Radius.circular(10),),
+    return Column(
+      children: [
+        Container(
+          height: 300,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10), bottomLeft: Radius.zero,bottomRight: Radius.circular(10),),
 
-      ),
+          ),
+        ),
+      ],
     );
   }
 }
