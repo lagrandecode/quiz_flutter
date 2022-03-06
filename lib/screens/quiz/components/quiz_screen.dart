a42b58ecc742d98276b39fadd442104f7c8f617d
ogunmolu oluwaseun <58033364+lagrandecode@users.noreply.github.com>
2022-03-06 17:12:23 +0100

    commit

--- lib/screens/quiz/components/quiz_screen.dart
+++ lib/screens/quiz/components/quiz_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({Key? key}) : super(key: key);
@@ -10,12 +13,38 @@ class QuizScreen extends StatefulWidget {
 class _QuizScreenState extends State<QuizScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Text("Score: 0", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-        ],
-      ),
+    return WillPopScope(
+        onWillPop: _onWillPop(),
+    child: Scaffold(
+    body: Column(
+    children: [
+    Text("Score: 0", style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+    ],
+    ),
+    ),
     );
   }
+
+  _onWillPop() async {
+    if(Platform.isAndroid){
+      final shouldPop = await showDialog(context: context, builder: (__) =>
+          AlertDialog(
+            title: Text("Alausasabi",),
+            content: Text("Are you sure You want to Exit?",),
+            actions: [
+              TextButton(onPressed: () {
+                Navigator.of(context).pop(false);
+              }, child: Text("cancel"),),
+              TextButton(onPressed: () {
+                Navigator.of(context).pop(true);
+              }, child: Text("ok"),),
+            ],
+          ),);
+
+      return shouldPop ?? false;
+    }
+    return false;
+
+  }
+
 }
